97e1707498c4e25de4f63df440f3758c4cf35a8a
Justiin <jwilcott@gmail.com>
2026-03-27 15:59:57 -0700

    added world geo

--- js/globe.js
+++ js/globe.js
@@ -1,4 +1,62 @@
-let scene, camera, renderer, sphere;
+let scene, camera, renderer, globeModel;
+
+function createFallbackGlobe() {
+    const geometry = new THREE.SphereGeometry(1, 32, 32);
+    const material = new THREE.MeshBasicMaterial({
+        color: 0x33ff33,
+        wireframe: true,
+        transparent: true,
+        opacity: 0.4
+    });
+
+    return new THREE.Mesh(geometry, material);
+}
+
+function styleLoadedGlobe(object) {
+    object.traverse((child) => {
+        if (child.isLine || child.isLineSegments) {
+            child.material = new THREE.LineBasicMaterial({
+                color: 0x33ff33,
+                transparent: true,
+                opacity: 0.85
+            });
+        } else if (child.isMesh) {
+            child.material = new THREE.MeshBasicMaterial({
+                color: 0x33ff33,
+                wireframe: true,
+                transparent: true,
+                opacity: 0.5
+            });
+        }
+    });
+
+    object.rotation.x = -0.15;
+    object.rotation.y = 0.3;
+    object.scale.setScalar(1.02);
+
+    return object;
+}
+
+function loadGlobeModel() {
+    const loader = new THREE.OBJLoader();
+
+    loader.load(
+        'assets/world-coastlines.obj',
+        (object) => {
+            if (globeModel) {
+                scene.remove(globeModel);
+            }
+
+            globeModel = styleLoadedGlobe(object);
+            scene.add(globeModel);
+        },
+        undefined,
+        () => {
+            globeModel = createFallbackGlobe();
+            scene.add(globeModel);
+        }
+    );
+}
 
 function initGlobe() {
     scene = new THREE.Scene();
@@ -13,23 +71,16 @@ function initGlobe() {
         antialias: true
     });
     renderer.setSize(container.clientWidth, container.clientHeight, false);
-    
-    const geometry = new THREE.SphereGeometry(1, 32, 32);
-    const material = new THREE.MeshBasicMaterial({
-        color: 0x33ff33,
-        wireframe: true,
-        transparent: true,
-        opacity: 0.8
-    });
-    sphere = new THREE.Mesh(geometry, material);
-    scene.add(sphere);
-    
+
+    loadGlobeModel();
     camera.position.z = 2.5;
 }
 
 function animateGlobe() {
     requestAnimationFrame(animateGlobe);
-    sphere.rotation.y += 0.005;
+    if (globeModel) {
+        globeModel.rotation.y += 0.005;
+    }
     renderer.render(scene, camera);
 }
 
